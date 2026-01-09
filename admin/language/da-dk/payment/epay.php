d6e3b6f92df699a12af461df4f7dc5018f00b3b0
Mads Monrad <mads@eventa.dk>
2026-01-09 14:26:32 +0100

    Added Danish language support

---
+++ admin/language/da-dk/payment/epay.php
@@ -0,0 +1,51 @@
+<?php
+// Heading
+$_['heading_title'] = 'ePay Payment Solutions';
+
+// Text
+$_['text_extension'] = 'Udvidelser';
+$_['text_success'] = 'Succes: Du har ændret ePay-indstillingerne!';
+$_['text_edit'] = 'Rediger ePay';
+$_['text_no_transaction'] = 'Der er endnu ikke modtaget nogen ePay-transaktion.';
+$_['text_transaction_id'] = 'Transaktions-ID';
+$_['text_transaction_state'] = 'Transaktionsstatus';
+$_['text_transaction_amount'] = 'Transaktionsbeløb';
+$_['text_transaction_captured'] = 'Hævet';
+$_['text_transaction_refunded'] = 'Refunderet';
+$_['text_transaction_voided'] = 'Annulleret';
+$_['text_transaction_updated'] = 'Sidst opdateret';
+$_['text_capture'] = 'Hævning';
+$_['text_refund'] = 'Refundering';
+$_['text_void'] = 'Annuller';
+$_['text_void_remaining'] = 'Annuller resterende beløb';
+$_['text_capture_success_title'] = 'Hævningsanmodning sendt.';
+$_['text_refund_success_title'] = 'Refunderingsanmodning sendt.';
+$_['text_void_success_title'] = 'Annulleringsanmodning sendt.';
+$_['text_capture_success'] = 'ePay-hævning anmodet for transaktion %s.';
+$_['text_refund_success'] = 'ePay-refundering anmodet for transaktion %s.';
+$_['text_void_success'] = 'ePay-annullering anmodet for transaktion %s.';
+$_['text_amount'] = 'Beløb';
+
+// Entry
+$_['entry_api_key'] = 'API-nøgle';
+$_['entry_point_of_sale_id'] = 'Point of Sale ID';
+$_['entry_webhook_auth'] = 'Webhook-godkendelse';
+$_['entry_order_status'] = 'Ordrestatus';
+$_['entry_failed_status'] = 'Fejlet ordrestatus';
+$_['entry_geo_zone'] = 'Geografisk zone';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sorteringsrækkefølge';
+
+// Error
+$_['error_permission'] = 'Advarsel: Du har ikke tilladelse til at ændre ePay!';
+$_['error_api_key'] = 'API-nøgle er påkrævet!';
+$_['error_point_of_sale_id'] = 'Point of Sale ID er påkrævet!';
+$_['error_webhook_auth'] = 'Webhook-godkendelse er påkrævet!';
+$_['error_order'] = 'Ordre ikke fundet!';
+$_['error_transaction_missing'] = 'Transaktions-ID mangler. Vent på webhook.';
+$_['error_amount'] = 'Indtast venligst et gyldigt beløb.';
+$_['error_capture_exceeds'] = 'Hævningsbeløbet overstiger det resterende autoriserede beløb.';
+$_['error_api'] = 'ePay API-forespørgslen mislykkedes.';
+
+$_['extension_heading_title'] = 'ePay Payment Solutions';
+$_['tab_epay'] = 'ePay Payment Solutions';
---
+++ catalog/language/da-dk/payment/epay.php
@@ -0,0 +1,15 @@
+<?php
+// Text
+$_['text_title'] = 'Kreditkort';
+$_['text_session_initialized'] = 'ePay-betalingssession initialiseret.';
+$_['text_webhook_success'] = 'ePay-betaling godkendt (transaktion %s).';
+$_['text_webhook_failed'] = 'ePay-betaling mislykkedes (transaktion %s).';
+
+// Button
+$_['button_confirm'] = 'Bekræft ordre';
+
+// Error
+$_['error_payment_method'] = 'Betalingsmetoden er forkert!';
+$_['error_order'] = 'Ordren mangler eller er ugyldig!';
+$_['error_api'] = 'Kan ikke initialisere betalingssessionen. Prøv venligst igen.';
+$_['error_configuration'] = 'Betalingsmetoden er ikke konfigureret.';
